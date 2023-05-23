8e0b88e30432ef22a3584d7e234af7cf9d314e6d
anvikahar123 <anvi@ditinteractive.com>
2023-05-23 21:04:29 +0530

    4 pages added

--- routes/pages_call.js
+++ routes/pages_call.js
@@ -10,4 +10,20 @@ router.get('/membership', async(req, res) => {
 router.get('/register', async(req, res) => {
     res.render('data_management/register')
 })
+
+router.get('/gamezone', async(req, res) => {
+    res.render('data_management/gamezone')
+})
+
+router.get('/restaurant', async(req, res) => {
+    res.render('data_management/restaurant')
+})
+
+router.get('/rooms', async(req, res) => {
+    res.render('data_management/rooms')
+})
+
+router.get('/spa', async(req, res) => {
+    res.render('data_management/spa')
+})
 module.exports = router;
